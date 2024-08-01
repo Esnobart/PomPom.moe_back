8b0a5cf351d24b5521b31adb8c6fd82e1fa2e2bb
Esnobart <offrichterrr@gmail.com>
2024-08-01 17:40:35 +0100

    start push, add index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.API_KEY).then(() => {console.log("Database connection successful")}).catch((err) => {console.log(err); process.exit(1)});
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/characters", characterRouter);
+
+app.use((_, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({ message });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running. Use our API on port: 3000");
+});
