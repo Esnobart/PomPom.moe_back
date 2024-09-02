ef96be7f19e116506ddc6ca9f101c755267cb907
Esnobart <offrichterrr@gmail.com>
2024-09-02 16:53:37 +0100

    update cones request

--- sevices/conesService.js
+++ sevices/conesService.js
@@ -1,18 +1,39 @@
 import { HSRchar } from "../models/charactersModel.js";
 import { HSRcones } from "../models/conesModel.js";
 
-async function getCones() {
+async function getConesWithChars() {
     try {
-        const list = await HSRcones.find().catch(err => {
-            console.error(err)
-            return null
+        // Получаем все элементы из коллекции HSRcones
+        const cones = await HSRcones.find().catch(err => {
+            console.error(err);
+            return null;
         });
-        return list
+
+        if (!cones) {
+            return null;
+        }
+
+        // Проходим по каждому конусу и находим соответствующих персонажей
+        const conesWithChars = await Promise.all(cones.map(async (cone) => {
+            const chars = await HSRchar.find({ cones: cone.id }).catch(err => {
+                console.error(err);
+                return [];
+            });
+
+            // Возвращаем конус с добавленным массивом персонажей
+            return {
+                ...cone._doc,  // Раскрываем все свойства текущего конуса
+                chars: chars   // Добавляем массив найденных персонажей
+            };
+        }));
+
+        return conesWithChars;
     } catch (err) {
         console.error(err.message);
-        return null
+        return null;
     }
 }
+
 
 async function getCone(id) {
     const cone = await HSRcones.findOne({ id: id }).catch(err => {
